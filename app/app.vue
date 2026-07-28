90c807b1ba1806fbc2896b00a3b1503ea253d05a
Eugene Bulgakov <zoom81@yandex.ru>
2026-07-28 13:36:24 +0400

    Add signup functionality with form and authentication client

--- app/app.vue
+++ app/app.vue
@@ -1,17 +1,5 @@
-<script lang="ts" setup>
-import { ref } from 'vue';
-const value1 = ref<number>(0);
-const value2 = ref<[number, number]>([20, 50]);
-</script>
-
 <template>
-  <div>
-    <a-flex gap="middle">
-      <a-button>Base button</a-button>
-      <a-button type="primary">Primary button</a-button>
-    </a-flex>
-
-    <a-slider id="test" v-model:value="value1"  />
-    <a-slider v-model:value="value2" range />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
