172991966acab659b76b2256ef0c7874bcd32da8
Eugene Bulgakov <zoom81@yandex.ru>
2026-07-29 00:03:41 +0400

    Add Header component and integrate it into app.vue

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
+<script lang="ts" setup>
+import { Header } from '~/widgets/header'
+</script>
+
 <template>
   <NuxtLayout>
+    <Header />
     <NuxtPage />
   </NuxtLayout>
 </template>
---
+++ app/widgets/header/ui/Header.vue
@@ -0,0 +1,23 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+  <a-flex class="header">
+    <a-flex align="center" gap="middle" class="username">
+      <span>UserName</span>
+      <a-button>Log Out</a-button>
+    </a-flex>
+  </a-flex>
+</template>
+
+
+<style lang="css" scoped>
+.header {
+  padding: 12px 24px;
+  border-bottom: 1px solid #f0f0f0;
+}
+
+.username {
+  margin-left: auto;
+}
+</style>
