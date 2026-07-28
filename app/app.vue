deadf9b97a6e8529f5ceaf1bac4f6f17071d3dfc
Eugene Bulgakov <zoom81@yandex.ru>
2026-07-28 11:18:07 +0400

    Add Ant Design

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,17 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+const value1 = ref<number>(0);
+const value2 = ref<[number, number]>([20, 50]);
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <a-flex gap="middle">
+      <a-button>Base button</a-button>
+      <a-button type="primary">Primary button</a-button>
+    </a-flex>
+
+    <a-slider id="test" v-model:value="value1"  />
+    <a-slider v-model:value="value2" range />
   </div>
 </template>
